74286de21025b5544b97beaa174adf2daddc6c72
hyroky867 <handm755@gmail.com>
2022-08-08 21:34:44 +0900

    feat: 14.11

--- main.go
+++ main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(":18080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	}))
-
-	if err != nil {
-		fmt.Printf("failad to terminate server: %v", err)
-		os.Exit(1)
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// 別ゴルーチンでHTTPサーバーを起動する
+	eg.Go(func() error {
+		// http.ErrServerClosedは http.ServerShutdown) が正常終了したことを示すので異常ではない
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルからの通知(終了通知)を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// Goメソッドで起動した別語ルーチンの終了を待つ
+	return eg.Wait()
 }
