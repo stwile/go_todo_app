d428e60804bc123cbf5fc3eecf24ad71a3820e67
hyroky867 <handm755@gmail.com>
2022-09-15 23:50:43 +0900

    feat: 18.21 ~ 18.22

--- store/task.go
+++ store/task.go
@@ -24,7 +24,7 @@ func (r *Repository) AddTask(ctx context.Context, db Execer, t *entity.Task) err
 	sql := `INSERT INTO task
 		(title, status, created, modified)
 	VALUES (?, ?, ?, ?)`
-	result, err := db.ExecContext(ctx, sql, t.Status, t.Created, t.Modified)
+	result, err := db.ExecContext(ctx, sql, t.Title, t.Status, t.Created, t.Modified)
 	if err != nil {
 		return err
 	}
--- store/task_test.go
+++ store/task_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/go-cmp/cmp"
+	"github.com/jmoiron/sqlx"
 
 	"github.com/stwile/go_todo_app/clock"
 	"github.com/stwile/go_todo_app/entity"
@@ -85,4 +87,35 @@ func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
 	wants[1].ID = entity.TaskID(id + 1)
 	wants[2].ID = entity.TaskID(id + 2)
 	return wants
+}
+
+func TestRepository_AddTask(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	c := clock.FixedClocker{}
+	var WantID int64 = 20
+	okTask := &entity.Task{
+		Title:    "ok task",
+		Status:   "todo",
+		Created:  c.Now(),
+		Modified: c.Now(),
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { db.Close() })
+	mock.ExpectExec(
+		// エスケープが必要
+		`INSERT INTO task \(title, status, created, modified\) VALUES \(\?, \?, \?, \?\)`,
+	).WithArgs(okTask.Title, okTask.Status, c.Now(), c.Now()).
+		WillReturnResult(sqlmock.NewResult(WantID, 1))
+
+	xdb := sqlx.NewDb(db, "mysql")
+	r := &Repository{Clocker: c}
+	if err := r.AddTask(ctx, xdb, okTask); err != nil {
+		t.Errorf("want no error, but got %v", err)
+	}
 }
